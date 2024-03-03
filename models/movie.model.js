73bcb2a1d959faed8669e78cfcce1a116cf5941c
cosmicwanderer7 <yewaleprithvi2003@gmail.com>
2024-03-03 23:31:10 +0530

    fixed models

--- models/movie.model.js
+++ models/movie.model.js
@@ -1,38 +1,31 @@
-const express = require('express');
-const app = express();
-const db = require("./app/models");
+const mongoose = require('mongoose');
 
-// Middleware
-app.use(express.json());
-
-// Require routes
-const movieRoutes = require('./routes/movie.routes');
-const userRoutes = require('./routes/user.routes');
-const artistRoutes = require('./routes/artist.routes');
-const genreRoutes = require('./routes/genre.routes');
-
-// Routes
-app.use('/movies', movieRoutes);
-app.use('/users', userRoutes);
-app.use('/artists', artistRoutes);
-app.use('/genres', genreRoutes);
-
-// Database connection setup
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    releaseDate: {
+        type: Date,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
+    // Add more fields as needed
 });
+
+module.exports = mongoose.model('Movie', movieSchema);
